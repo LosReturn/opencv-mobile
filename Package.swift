43c641e9aa8be85d1d6e7ea93719d296b6b39f73
LosReturn <loves0510@outlook.com>
2023-04-03 16:35:43 +0800

    Create Package.swift

---
+++ Package.swift
@@ -0,0 +1,33 @@
+import PackageDescription
+
+let version = "4.7.0"
+let checksum = "7e578f7734ff25796acc2847163c86c857a11351a2f99b220d11189639786ee9"
+
+let package = Package(
+    name: "OpenCV",
+    platforms: [
+        .macOS(.v10_13), .iOS(.v11), .macCatalyst(.v13)
+    ],
+    products: [
+        .library(
+            name: "OpenCV",
+            targets: ["opencv2", "opencv2-dependencies"]),
+    ],
+    targets: [
+        .binaryTarget(name: "opencv2",
+                      url: "https://github.com/yeatse/opencv-spm/releases/download/\(version)/opencv2.xcframework.zip",
+                      checksum: checksum),
+        .target(
+            name: "opencv2-dependencies",
+            linkerSettings: [
+                .linkedFramework("AVFoundation"),
+                .linkedFramework("CoreImage"),
+                .linkedFramework("CoreMedia"),
+                .linkedFramework("CoreVideo", .when(platforms: [.iOS])),
+                .linkedFramework("Accelerate", .when(platforms: [.iOS, .macOS])),
+                .linkedFramework("OpenCL", .when(platforms: [.macOS])),
+                .linkedLibrary("c++")
+            ]
+        )
+    ]
+)
