02cbf2977d189c7c25e616ec9dd740f0b92bc07f
LosReturn <loves0510@outlook.com>
2023-04-20 11:28:52 +0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,7 +1,7 @@
 // swift-tools-version: 5.7
 import PackageDescription
 
-let version = "Release v16"
+let version = "v16"
 let checksum = "1ead050146646a8f81e37aba0f7efb7033678311734a272f76557647c4d419b2"
 
 let package = Package(
